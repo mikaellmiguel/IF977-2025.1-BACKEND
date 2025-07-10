bb60cc4d8ec4d2677ba70ea1fed11f2185156256
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2025-07-09 22:53:06 -0300

    feat: adiciona a importação das rotas no server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,14 @@
 require('dotenv').config();
-require("express-async-errors");
+require("express-async-error");
 
 const express = require('express');
 const errorHandler = require('./middlewares/errorHandler');
+const routes = require('./routes');
 
 const app = express();
 app.use(express.json());
 
-// ROTA INICIAL PARA TESTE
-app.get('/', (req, res) => {
-  res.send({
-    message: 'Bem vindo ao servidor Express!',
-  });
-});
+app.use(routes);
 
 app.use(errorHandler);
 
