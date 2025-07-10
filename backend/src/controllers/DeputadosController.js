718e127bc9185cc7bb0ad6776a3b016162110926
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2025-07-09 23:14:56 -0300

    feat: implementa método show para exibir detalhes de deputados

--- backend/src/controllers/DeputadosController.js
+++ backend/src/controllers/DeputadosController.js
@@ -3,6 +3,12 @@ const {getDetailsDeputadoById} = require('../services/camara.service');
 
 class DeputadosController {
 
+    async show(request, response) {
+        const { id } = request.params;
+        const deputado = await getDetailsDeputadoById(id);
+        return response.json(deputado);
+    }
+
     
 }
 
