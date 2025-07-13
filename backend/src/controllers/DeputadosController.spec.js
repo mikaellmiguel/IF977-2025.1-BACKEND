a36fae322c2544676ce789a2db3c68453edbc6dc
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2025-07-13 13:12:07 -0300

    test: adiciona teste para verificar retorno correto da listagem de deputadfos

--- backend/src/controllers/DeputadosController.spec.js
+++ backend/src/controllers/DeputadosController.spec.js
@@ -4,16 +4,29 @@ const AppError = require('../utils/AppError');
 
 jest.mock('../services/camara.service');
 
+
+const mockKnexInstance = {
+    count: jest.fn().mockReturnThis(),
+    orderBy: jest.fn().mockReturnThis(),
+    limit: jest.fn().mockReturnThis(),
+    offset: jest.fn().mockReturnThis(),
+    select: jest.fn(),
+    first: jest.fn()
+};
+
+jest.mock("../database/knex", () => jest.fn((tableName) => mockKnexInstance)) ;
+
 describe('DeputadosController', () => {
     let request, response;
 
-    beforeEach(() => {
-        request = { params: { id: '204555' } };
-        response = { json: jest.fn() };
-    });
+    describe('Metodo show', () => {
 
-    describe('show', () => {
+        beforeEach(() => {
+            request = { params: { id: '204555' } };
+            response = { json: jest.fn() };
+        });
 
+        
         it('deve retornar os dados do deputado quando encontrado', async () => {
             const mockDeputado = { id: 204555, nome: 'Marcos Pereira' };
             getDetailsDeputadoById.mockResolvedValue(mockDeputado);
@@ -34,6 +47,52 @@ describe('DeputadosController', () => {
                 expect(err.message).toBe('Deputado não encontrado');
                 expect(err.statusCode).toBe(400);
             }
+        });
+    
+    });
+
+    describe('Metodo index', () => {
+        
+        let controller;
+        let knexMockFn;
+        let response;
+
+        beforeEach(() => {
+            controller = new DeputadosController();
+            knexMockFn = require('../database/knex');
+            response = { json: jest.fn() };
+            jest.clearAllMocks();
+        });
+
+        it('deve retornar lista de deputados com total, limit e offset', async () => {
+
+            mockKnexInstance.first.mockResolvedValue({ total: 2 });
+
+            mockKnexInstance.orderBy("nome").limit(2).offset(0).select.mockResolvedValue([
+                { id: 1, nome: 'Deputado 1', partido: 'ABC', sigla_uf: 'PE', url_foto: '', email: '', data_nascimento: '1980-01-01' },
+                { id: 2, nome: 'Deputado 2', partido: 'DEF', sigla_uf: 'SP', url_foto: '', email: '', data_nascimento: '1985-01-01' }
+            ]);
+
+            const request = { query: { limit: '2', offset: '0' } };
+
+            await controller.index(request, response);
+            
+            expect(knexMockFn("deputados").count).toHaveBeenCalledWith('* as total');
+            expect(await knexMockFn("deputados").count("* as total").first()).toEqual({total: 2});
+            expect(knexMockFn("deputados").orderBy).toHaveBeenCalledWith("nome");
+            expect(knexMockFn("deputados").limit).toHaveBeenCalledWith(2);
+            expect(knexMockFn("deputados").offset).toHaveBeenCalledWith(0);
+            expect(knexMockFn("deputados").select).toHaveBeenCalledWith("id", "nome", "partido", "sigla_uf", "url_foto", "email", "data_nascimento");
+
+            expect(response.json).toHaveBeenCalledWith({
+                dados: [
+                    { id: 1, nome: 'Deputado 1', partido: 'ABC', sigla_uf: 'PE', url_foto: '', email: '', data_nascimento: '1980-01-01' },
+                    { id: 2, nome: 'Deputado 2', partido: 'DEF', sigla_uf: 'SP', url_foto: '', email: '', data_nascimento: '1985-01-01' }
+                ],
+                total: 2,
+                limit: 2,
+                offset: 0
+            });
         });
     });
 });
