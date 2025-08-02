da30157272260d5ce8f600a47f609b7ec9b000da
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2025-08-02 16:32:25 -0300

    feat: atualiza método index do DespesasController para incluir total, limite e offset na resposta

--- backend/src/controllers/DespesasController.js
+++ backend/src/controllers/DespesasController.js
@@ -11,10 +11,17 @@ class DespesasController {
         const limit = request.query.limit ? Number(request.query.limit) : 10;
         const offset = request.query.offset ? Number(request.query.offset) : 0;
 
-        const despesas = await knex('despesas').select('descricao', 'valor_documento', 'data_emissao', 'fornecedor', 'sigla_uf')
-                                .where('id_deputado', deputado_id).orderBy('data_emissao', 'desc');
+        const {total} = await knex('despesas').where('id_deputado', deputado_id).count('* as total').first();
 
-        response.json(despesas);
+        const despesas = await knex('despesas').select('descricao', 'valor_documento', 'data_emissao', 'fornecedor', 'sigla_uf')
+                                .where('id_deputado', deputado_id).orderBy('data_emissao', 'desc').limit(limit).offset(offset);
+
+        response.json({
+            dados: despesas,
+            total: total,
+            limit: limit,
+            offset: offset
+        });
     }
 
 }
