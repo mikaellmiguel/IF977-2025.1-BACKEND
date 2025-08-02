4d7fa854945b418cbcd5b092d96e8abcee1b6114
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2025-08-02 10:18:47 -0300

    feat: adiciona colunas id_documento e trecho na tabela despesas e ajusta a chave única

--- backend/src/database/knex/migrations/20250625115159_createDespesas.js
+++ backend/src/database/knex/migrations/20250625115159_createDespesas.js
@@ -16,11 +16,14 @@ exports.up = knex => knex.schema.createTable("despesas", table => {
     table.decimal("valor_documento", 10, 2).notNullable();
     table.integer("mes").notNullable();
     table.integer("ano").notNullable();
+    table.integer("id_documento").notNullable();
     table.string("url_documento").nullable();
+    table.string("trecho").nullable();
+    table.unique(["id_deputado", "numero_documento", "data_emissao", "valor_documento", "id_documento", "trecho"]);
 });
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = knex => knex.schema.dropTable("depesas"); 
+exports.down = knex => knex.schema.dropTable("despesas"); 
