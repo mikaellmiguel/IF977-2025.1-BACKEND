9eb6c8e2402de63e9436acf7cd0347571e5414c3
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2025-08-02 14:22:31 -0300

    feat: adiciona a função validarIdDeputado para validação de IDs de deputados

---
+++ backend/src/utils/validarIdDeputado.js
@@ -0,0 +1,19 @@
+const AppError = require('./AppError');
+const knex = require('../database/knex');
+
+async function validarIdDeputado(deputado_id) {
+
+    deputado_id = Number(deputado_id);
+
+    if (!Number.isInteger(deputado_id) || deputado_id <= 0) {
+        throw new AppError("Parâmetro 'deputado_id' deve ser um número inteiro positivo", 400);
+    }
+
+    const deputado = await knex('deputados').where('id', deputado_id).first();
+
+    if (!deputado) {
+        throw new AppError(`Deputado com ID ${deputado_id} não encontrado`, 404);
+    }
+}
+
+module.exports = validarIdDeputado;
