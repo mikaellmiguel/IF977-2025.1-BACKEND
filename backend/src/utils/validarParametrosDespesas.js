bd51ad84891c4253edd274e02cacdd88e4cf71d0
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2025-08-03 13:41:17 -0300

    feat: adiciona funções de validação para parâmetros de despesas

---
+++ backend/src/utils/validarParametrosDespesas.js
@@ -0,0 +1,39 @@
+const AppError = require("../utils/AppError");
+
+function validarLimitOffset(limit, offset) {
+
+    limit = limit ? Number(limit) : 10; // valor padrão
+    offset = offset ? Number(offset) : 0; // valor padrão
+    
+    if (!/^\d+$/.test(limit) || !/^\d+$/.test(offset)) {
+        throw new AppError("Parâmetros 'limit' e 'offset' devem ser números inteiros positivos", 400);
+    }
+    return true;
+}
+
+function validarValores(valor_min, valor_max) {
+    if (valor_min && (Number(valor_min) < 0 || Number.isNaN(Number(valor_min)))) {
+        throw new AppError("Parâmetro 'valor_min' deve ser um número válido e positivo", 400);
+    }
+    if (valor_max && (Number(valor_max) < 0 || Number.isNaN(Number(valor_max)))) {
+        throw new AppError("Parâmetro 'valor_max' deve ser um número válido e positivo", 400);
+    }
+    if (Number(valor_min) > Number(valor_max)) {
+        throw new AppError("Parâmetro 'valor_min' não pode ser maior que 'valor_max'", 400);
+    }
+
+    return true;
+}
+
+function validarTipo(tipo) {
+    if (tipo && typeof tipo !== 'string') {
+        throw new AppError("Parâmetro 'tipo' deve ser uma string", 400);
+    }
+    return true;
+}
+
+module.exports = {
+    validarLimitOffset,
+    validarValores,
+    validarTipo
+};
