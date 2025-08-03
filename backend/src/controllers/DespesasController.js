08418421aafd62b97512cd5c89b3c985135cc4d5
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2025-08-03 14:56:10 -0300

    feat: adiciona método de ranking de despesas dos deputados

--- backend/src/controllers/DespesasController.js
+++ backend/src/controllers/DespesasController.js
@@ -35,5 +35,38 @@ class DespesasController {
             offset: offset
         });
     }
+
+     async ranking(request, response) {
+
+        const { limit, offset } = validarLimitOffset(request.query.limit, request.query.offset);
+
+        const ranking = await knex("deputados")
+            .join("despesas", "despesas.id_deputado", "deputados.id")
+            .select(
+                "deputados.id as id",
+                "deputados.nome as nome",
+                "deputados.url_foto as url_foto",
+                "deputados.partido as partido",
+                "deputados.sigla_uf as uf",
+                knex.raw("SUM(despesas.valor_documento) as total_gasto")
+            )
+            .groupBy("deputados.id")
+            .orderBy("total_gasto", "desc")
+            .limit(limit)
+            .offset(offset);
+
+        // total de deputados com despesas
+        const { total } = await knex("despesas")
+            .join("deputados", "deputados.id", "despesas.id_deputado")
+            .countDistinct("deputados.id as total").first();
+
+        response.json({
+            dados: ranking,
+            total: Number(total) ? Number(total) : 0,
+            limit:limit,
+            offset: offset
+        });
+    }
+
 }
 module.exports = DespesasController;
