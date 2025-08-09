282f71801e71ce97f6252b9245a0412ab7bc730d
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2025-08-09 11:02:52 -0300

    feat: adiciona ReferenciasController com dependências necessárias

---
+++ backend/src/controllers/ReferenciasController.js
@@ -0,0 +1,8 @@
+const knex = require("../database/knex");
+const validarIdDeputado = require('../utils/validarIdDeputado');
+
+class ReferenciasController {
+
+}
+
+module.exports =  ReferenciasController;
