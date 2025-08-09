376291814e4d2a507c51e410eff9d610adfe5218
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2025-08-09 11:03:02 -0300

    feat: implementa método getReferenciasDespesas no ReferenciasController

--- backend/src/controllers/ReferenciasController.js
+++ backend/src/controllers/ReferenciasController.js
@@ -3,6 +3,24 @@ const validarIdDeputado = require('../utils/validarIdDeputado');
 
 class ReferenciasController {
 
+    async getReferenciasDespesas(request, response) {
+        const {id} = request.query;
+        let query = knex("despesas");
+
+        if (id) {
+            await validarIdDeputado(id);
+            query = query.where("id_deputado", id);
+        }
+
+        const tipos = await query.clone().distinct("descricao").pluck("descricao");
+        const estados = await query.clone().distinct("sigla_uf").pluck("sigla_uf");
+
+        return response.json({
+            tipos: tipos,
+            estados: estados
+        });
+    }
+
 }
 
 module.exports =  ReferenciasController;
