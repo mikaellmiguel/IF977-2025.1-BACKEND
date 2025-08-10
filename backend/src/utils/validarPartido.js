4612bea8b15a9e2b6194815bc9bc24543d0500f3
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2025-08-10 18:26:24 -0300

    fix: ajusta validação do parâmetro 'partido' para aceitar letras maiúsculas e minúsculas

--- backend/src/utils/validarPartido.js
+++ backend/src/utils/validarPartido.js
@@ -2,14 +2,14 @@ const knex = require('../database/knex');
 const AppError = require('../utils/AppError');
 
 async function validarPartido(partido) {
-    if (!/^[A-Z]{2,5}$/.test(partido)) {
+    if (!/^[A-Za-z]{2,15}$/.test(partido)) {
         throw new AppError("Parâmetro 'partido' inválido", 400);
     }
 
     const partidosDb = await knex("deputados").distinct("partido");
-    const partidos_validos = partidosDb.map(p => p.partido);
+    const partidos_validos = partidosDb.map(p => p.partido.toUpperCase());
 
-    if (!partidos_validos.includes(partido)) {
+    if (!partidos_validos.includes(partido.toUpperCase())) {
         throw new AppError("Partido não encontrado", 400);
     }
 
