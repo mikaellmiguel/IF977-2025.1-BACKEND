06330eac3232b2ea732c8e26ba5bedc4b8adb08c
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2025-08-10 20:51:56 -0300

    refactor: remove método index do DeputadosController e suas referências nas rotas, ficará apenas o search

--- backend/src/controllers/DeputadosController.js
+++ backend/src/controllers/DeputadosController.js
@@ -6,37 +6,6 @@ const validarSiglaUf = require('../utils/validarSiglaUf');
 const validarIdDeputado = require('../utils/validarIdDeputado');
 
 class DeputadosController {
-
-    async index(request, response) {
-
-        const limit = request.query.limit || 20;
-        const offset = request.query.offset || 0;
-
-        //  Verifica se os parâmetros limit e offset são números inteiros positivos
-        if(!/^\d+$/.test(limit) || !/^\d+$/.test(offset)) {
-            throw new AppError("Parâmetros 'limit' e 'offset' devem ser números inteiros positivos", 400);
-        }
-
-        try {
-            const {total} = await knex("deputados").count('* as total').first();
-
-            const deputados = await knex("deputados")
-                                    .orderBy("nome")
-                                    .limit(limit)
-                                    .offset(offset)
-                                    .select("id", "nome", "partido", "sigla_uf", "url_foto", "email", "data_nascimento");
-            
-            return response.json({
-                dados: deputados,
-                total: total,
-                limit: parseInt(limit),
-                offset: parseInt(offset)
-            });
-        }
-        catch (error) {
-            throw new Error(`ERROR (DeputadosController/index): Erro ao buscar deputados: ${error.message}`);
-        }
-    }
     
     async show(request, response) {
         const { id } = request.params;
