17feebc8cbc94d90fb01a96f2e5b39ef14b2e1b1
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2025-08-12 20:05:47 -0300

    fix: corrige tipo da coluna 'descricao' e ajusta 'data_emissao' para ser nullable

--- backend/src/database/knex/migrations/20250625115159_createDespesas.js
+++ backend/src/database/knex/migrations/20250625115159_createDespesas.js
@@ -8,11 +8,11 @@ exports.up = knex => knex.schema.createTable("despesas", table => {
     table.string("numero_carteira").notNullable();
     table.string("sigla_uf");
     table.string("sigla_partido");
-    table.integer("descricao").notNullable();
+    table.string("descricao").notNullable();
     table.string("fornecedor").notNullable();
     table.string("cnpj").nullable();
     table.string("numero_documento").notNullable();
-    table.timestamp("data_emissao").notNullable();
+    table.timestamp("data_emissao").nullable();
     table.decimal("valor_documento", 10, 2).notNullable();
     table.integer("mes").notNullable();
     table.integer("ano").notNullable();
